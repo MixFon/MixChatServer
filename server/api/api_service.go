27ad4a1121003e93902d601019a834568159778b
mixfon <mixfon@rambler.ru>
2023-08-12 22:30:02 +0300

    Рефакторинг

--- server/api/api_service.go
+++ server/api/api_service.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"server/models"
 	"time"
@@ -35,8 +34,8 @@ func (api *APIService) SetDatabase(database DatabaseInterfase) {
 func (api APIService) GetAllChannels(w http.ResponseWriter, r *http.Request) {
 	dbChannels, err := api.database.GetAllChannels()
 	if err != nil {
-		fmt.Println("Error get Channels")
-		log.Fatalln("unable marshal to json")
+		sendError(err, w, r)
+		return
 	}
 	if len(dbChannels) == 0 {
 		sendEmptySliceChannels(w, r)
@@ -47,31 +46,26 @@ func (api APIService) GetAllChannels(w http.ResponseWriter, r *http.Request) {
 
 // Добавление нового канала, присваивает каналу id
 func (api APIService) AddNewChannel(w http.ResponseWriter, r *http.Request) {
-	// Прочитать тело запроса
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, "Ошибка при чтении запроса", http.StatusInternalServerError)
+		sendError(err, w, r)
 		return
 	}
 
 	var newChannel models.Channel
 
-	// Декодировать JSON в структуру Channel
 	err = json.Unmarshal(body, &newChannel)
 	if err != nil {
-		http.Error(w, "Ошибка при демаршалинге JSON", http.StatusBadRequest)
+		sendError(err, w, r)
 		return
 	}
 
-	// В этом моменте у вас есть новая структура Channel в переменной newChannel.
-	// Здесь вы можете произвести сохранение новой структуры в базу данных или в другое место по вашему выбору.
-	// Например, можно добавить ее в существующий слайс или сохранить в базу данных.
 	id := uuid.New()
 	newChannel.Id = id.String()
 	newChannel.LastMessage = nil
 	newChannel.LastActivity = nil
 	if err = api.database.AddChannel(newChannel); err != nil {
-		http.Error(w, "Ошибка сохранения в базу данных", http.StatusBadRequest)
+		sendError(err, w, r)
 		return
 	}
 	sendChannel(newChannel, w, r)
@@ -84,7 +78,7 @@ func (api APIService) DeleteChannel(w http.ResponseWriter, r *http.Request) {
 
 	err := api.database.DelegeChannel(channelID)
 	if err != nil {
-		http.Error(w, "ошибка удаления канала", http.StatusBadRequest)
+		sendError(err, w, r)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
@@ -98,7 +92,7 @@ func (api APIService) GetChannel(w http.ResponseWriter, r *http.Request) {
 
 	channel, err := api.database.GetChannel(channelID)
 	if err != nil {
-		http.Error(w, "Канал не найден", http.StatusBadRequest)
+		sendError(err, w, r)
 		return
 	}
 	sendChannel(channel, w, r)
@@ -111,14 +105,15 @@ func (api APIService) GetMessagesChannel(w http.ResponseWriter, r *http.Request)
 
 	messages, err := api.database.GetAllMessages(channelID)
 	if err != nil {
-		log.Fatalln("unable marshal to json")
+		sendError(err, w, r)
+		return
 	}
 
 	jsMessages, err := json.Marshal(messages)
 	if err != nil {
-		log.Fatalln("unable marshal to json")
+		sendError(err, w, r)
+		return
 	}
-	// Устанавливаем заголовки HTTP для JSON
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
@@ -127,10 +122,9 @@ func (api APIService) GetMessagesChannel(w http.ResponseWriter, r *http.Request)
 
 // Получение нового сообщения в канал
 func (api APIService) MessageChannel(w http.ResponseWriter, r *http.Request) {
-	// Прочитать тело запроса
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, "Ошибка при чтении запроса", http.StatusInternalServerError)
+		sendError(err, w, r)
 		return
 	}
 
@@ -138,11 +132,9 @@ func (api APIService) MessageChannel(w http.ResponseWriter, r *http.Request) {
 	channelID := vars["channelID"]
 
 	var newMessage models.Message
-
-	// Декодировать JSON в структуру Channel
 	err = json.Unmarshal(body, &newMessage)
 	if err != nil {
-		http.Error(w, "Ошибка при демаршалинге JSON", http.StatusBadRequest)
+		sendError(err, w, r)
 		return
 	}
 
@@ -153,30 +145,25 @@ func (api APIService) MessageChannel(w http.ResponseWriter, r *http.Request) {
 	// Форматирование в стандарт ISO 8601
 	iso8601Format := "2006-01-02T15:04:05-07:00"
 	iso8601Time := currentTime.Format(iso8601Format)
-	fmt.Println("ISO 8601 формат:", iso8601Time)
-
 	parsedTime, err := time.Parse(iso8601Format, iso8601Time)
 	if err != nil {
-		fmt.Println("Ошибка при разборе времени:", err)
+		sendError(err, w, r)
 		return
 	}
 
 	newMessage.Date = parsedTime
-
 	jsMessage, err := json.Marshal(newMessage)
 	if err != nil {
-		fmt.Println("Err %w", err)
-		log.Fatalln("unable marshal to json")
+		sendError(err, w, r)
+		return
 	}
 
 	err = api.database.AddMessage(channelID, newMessage)
 	if err != nil {
-		http.Error(w, "Ошибка добавления сообщеня в базу данных", http.StatusInternalServerError)
+		sendError(err, w, r)
 		return
 	}
-	fmt.Println(string(jsMessage))
 
-	// Устанавливаем заголовки HTTP для JSON
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsMessage)
@@ -188,10 +175,9 @@ func (api APIService) MessageChannel(w http.ResponseWriter, r *http.Request) {
 func sendChannel(channel models.Channel, w http.ResponseWriter, r *http.Request) {
 	jsChannel, err := json.Marshal(channel)
 	if err != nil {
-		fmt.Println("!!!Err %w", err)
-		log.Fatalln("unable marshal to json!!!!")
+		sendError(err, w, r)
+		return
 	}
-	// Устанавливаем заголовки HTTP для JSON
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
@@ -202,10 +188,9 @@ func sendChannel(channel models.Channel, w http.ResponseWriter, r *http.Request)
 func sendSliceChannels(channels []models.Channel, w http.ResponseWriter, r *http.Request) {
 	jsChannels, err := json.Marshal(channels)
 	if err != nil {
-		fmt.Println("Error Marshaling Channels")
-		log.Fatalln("unable marshal to json")
+		sendError(err, w, r)
+		return
 	}
-	// Устанавливаем заголовки HTTP для JSON
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
@@ -215,14 +200,17 @@ func sendSliceChannels(channels []models.Channel, w http.ResponseWriter, r *http
 // Отправка пустого слайса каналов
 func sendEmptySliceChannels(w http.ResponseWriter, r *http.Request) {
 	emptyArray := []models.Channel{}
-
-	// Преобразуем массив в JSON
 	responseJSON, err := json.Marshal(emptyArray)
 	if err != nil {
-		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+		sendError(err, w, r)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(responseJSON)
+}
+
+// Отправка ошибки
+func sendError(err error, w http.ResponseWriter, r *http.Request) {
+	http.Error(w, fmt.Sprint("%w\n", err), http.StatusInternalServerError)
 }
